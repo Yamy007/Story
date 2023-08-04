6d2e66a636ac88e88d27fc77be70e7fc177a51ab
babygrim <bstetskiv@gmail.com>
2023-08-04 14:12:03 +0300

    new init

--- Backend/AppBackend/asgi.py
+++ Backend/AppBackend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for appBackend project.
+ASGI config for AppBackend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'appBackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AppBackend.settings')
 
 application = get_asgi_application()
--- Backend/backend/views.py
+++ Backend/backend/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from django.http import JsonResponse
+# Create your views here.
 
 def home(request):
-    array = [1,2,3,4,5,6,7,8,9,10]
+    array = [1,2,3,4,5,6,6]
     return JsonResponse(array, safe=False)
