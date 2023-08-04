6dfc93396db916f767d09139ea32f41e05e94f03
babygrim <bstetskiv@gmail.com>
2023-08-04 13:52:00 +0300

    test api

--- Backend/appBackend/Backend/views.py
+++ Backend/appBackend/Backend/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def home(request):
+    array = [1,2,3,4,5,6,7,8,9,10]
+    return JsonResponse(array, safe=False)
