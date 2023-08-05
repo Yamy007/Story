44df5d1cb43224cd93403c14b6ceaee1b401ee70
Yamy <yamy19007@gmail.com>
2023-08-05 14:39:47 +0200

    info story

--- Frontend/frontend/src/components/Story/StoryInfo.js
+++ Frontend/frontend/src/components/Story/StoryInfo.js
@@ -1,87 +1,46 @@
 import React, { useState, useEffect } from 'react'
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, Pagination } from '@mui/material'
 import { useParams } from 'react-router-dom'
 import styles from './Style/Style.module.css'
 import { api } from '../api/api'
+
 export const StoryInfo = ({ isDark }) => {
 	const { id } = useParams()
-	console.log('id', id)
 	const [story, setStory] = useState()
-
+	const [page, setPage] = useState(1)
+	const [pg, setPg] = useState(null)
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
-				const response = await api.get(`get_all_posts?story=${id}`)
-				console.log('response', response.data)
+				const response = await api.get(
+					`get_all_posts?story=${id}&story_page=${page}`
+				)
+				setStory(response.data.story_data)
+				setPg(response.data.story_page)
 			} catch (err) {
 				console.log(err)
 			}
 		}
 		fetchData()
-	}, [])
+	}, [page])
+
 	return (
 		<Box bgcolor={isDark ? '#000000e8' : '#EBEBEB'} className={styles.wrapper}>
 			<Box className={styles.content}>
 				<Typography variant='h5' component='p'>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis commodi
-					sequi consequatur dolor incidunt, deserunt ad illo maxime ipsum minus
-					quod repudiandae amet natus modi aut aspernatur ut voluptates nisi?
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis commodi
-					sequi consequatur dolor incidunt, deserunt ad illo maxime ipsum minus
-					quod repudiandae amet natus modi aut aspernatur ut voluptates nisi?
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis commodi
-					sequi consequatur dolor incidunt, deserunt ad illo maxime ipsum minus
-					quod repudiandae amet natus modi aut aspernatur ut voluptates nisi?
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis commodi
-					sequi consequatur dolor incidunt, deserunt ad illo maxime ipsum minus
-					quod repudiandae amet natus modi aut aspernatur ut voluptates nisi?
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis commodi
-					sequi consequatur dolor incidunt, deserunt ad illo maxime ipsum minus
-					quod repudiandae amet natus modi aut aspernatur ut voluptates nisi?
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis commodi
-					sequi consequatur dolor incidunt, deserunt ad illo maxime ipsum minus
-					quod repudiandae amet natus modi aut aspernatur ut voluptates nisi?
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis commodi
-					sequi consequatur dolor incidunt, deserunt ad illo maxime ipsum minus
-					quod repudiandae amet natus modi aut aspernatur ut voluptates nisi?
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis commodi
-					sequi consequatur dolor incidunt, deserunt ad illo maxime ipsum minus
-					quod repudiandae amet natus modi aut aspernatur ut voluptates nisi?
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis commodi
-					sequi consequatur dolor incidunt, deserunt ad illo maxime ipsum minus
-					quod repudiandae amet natus modi aut aspernatur ut voluptates nisi?
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis commodi
-					sequi consequatur dolor incidunt, deserunt ad illo maxime ipsum minus
-					quod repudiandae amet natus modi aut aspernatur ut voluptates nisi?
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis commodi
-					sequi consequatur dolor incidunt, deserunt ad illo maxime ipsum minus
-					quod repudiandae amet natus modi aut aspernatur ut voluptates nisi?
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis commodi
-					sequi consequatur dolor incidunt, deserunt ad illo maxime ipsum minus
-					quod repudiandae amet natus modi aut aspernatur ut voluptates nisi?
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis commodi
-					sequi consequatur dolor incidunt, deserunt ad illo maxime ipsum minus
-					quod repudiandae amet natus modi aut aspernatur ut voluptates nisi?
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis commodi
-					sequi consequatur dolor incidunt, deserunt ad illo maxime ipsum minus
-					quod repudiandae amet natus modi aut aspernatur ut voluptates nisi?
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis commodi
-					sequi consequatur dolor incidunt, deserunt ad illo maxime ipsum minus
-					quod repudiandae amet natus modi aut aspernatur ut voluptates nisi?
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis commodi
-					sequi consequatur dolor incidunt, deserunt ad illo maxime ipsum minus
-					quod repudiandae amet natus modi aut aspernatur ut voluptates nisi?
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis commodi
-					sequi consequatur dolor incidunt, deserunt ad illo maxime ipsum minus
-					quod repudiandae amet natus modi aut aspernatur ut voluptates nisi?
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis commodi
-					sequi consequatur dolor incidunt, deserunt ad illo maxime ipsum minus
-					quod repudiandae amet natus modi aut aspernatur ut voluptates nisi?
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis commodi
-					sequi consequatur dolor incidunt, deserunt ad illo maxime ipsum minus
-					quod repudiandae amet natus modi aut aspernatur ut voluptates nisi?
+					{story?.body[0]}
 				</Typography>
 			</Box>
+			<Pagination
+				variant='outlined'
+				color='secondary'
+				sx={{ marginBottom: '15px' }}
+				count={pg?.story_page_count}
+				page={page}
+				onChange={(e, newPage) => {
+					setPage(newPage)
+				}}
+			/>
 		</Box>
 	)
 }
