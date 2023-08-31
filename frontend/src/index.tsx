029d71a306940e2f7c9379a8bad2fa9ae6be0d26
YevhenP <panchoshnuyevgeniy@outlook.com>
2023-08-31 20:04:11 +0300

    correction Header.module.scss Header.tsx LoginComponent.tsx LoginComponent.module.scss LoginPage.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
+import "./styles.scss";
 import { router } from "./router";
 import { Provider } from "react-redux";
 import { store } from "./redux";
@@ -13,7 +14,3 @@ root.render(
     <RouterProvider router={router} />
   </Provider>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
