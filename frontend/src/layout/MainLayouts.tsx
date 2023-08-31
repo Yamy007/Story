eb88b23e8999745e1c5da713cb9e719bb12131a2
YevhenP <panchoshnuyevgeniy@outlook.com>
2023-09-01 00:43:14 +0300

    update LoginComponent.module.scss MainLayouts.tsx styles.scss

--- frontend/src/layout/MainLayouts.tsx
+++ frontend/src/layout/MainLayouts.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { Header } from "../components/Header/Header";
+import { Header } from "../components";
 
 export const MainLayouts = () => {
   return (
-    <div>
+    <>
       <Header />
       <Outlet />
-    </div>
+    </>
   );
 };
