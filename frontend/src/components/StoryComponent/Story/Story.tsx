df91a306facb579d75a661eaa0b1f53f9e1e7351
Yamy <yamy19007@gmail.com>
2023-09-01 15:24:41 +0200

    add interface add iccons

--- frontend/src/components/StoryComponent/Story/Story.tsx
+++ frontend/src/components/StoryComponent/Story/Story.tsx
@@ -1,22 +1,33 @@
-import { FC, PropsWithChildren } from "react";
-import { CardContent, CardHeader, Card } from "@mui/material";
-
-import { IStory } from "../../../interface";
+import { FC, PropsWithChildren } from 'react'
+import {
+	CardContent,
+	CardHeader,
+	Card,
+	Button,
+	IconButton,
+} from '@mui/material'
+import { IStory } from '../../../interface'
+import FavoriteIcon from '@mui/icons-material/Favorite'
 
 export interface StoryPropsInterface extends PropsWithChildren {
-  story: IStory;
+	story: IStory
 }
 
 export const Story: FC<StoryPropsInterface> = ({ story }) => {
-  const { title, story_body, date, likes } = story;
+	const { title, story_body, date, likes } = story
 
-  const dateTransform = (date: string) => new Date(date).toLocaleDateString();
+	const dateTransform = (date: string) => new Date(date).toLocaleDateString()
 
-  return (
-    <Card>
-      <CardHeader title={title} subheader={dateTransform(date)} />
-      <CardContent>{story_body}</CardContent>
-      <CardContent>Likes: {likes}</CardContent>
-    </Card>
-  );
-};
+	return (
+		<Card>
+			<CardHeader title={title} subheader={dateTransform(date)} />
+			<CardContent>{story_body}</CardContent>
+			<CardContent>
+				<IconButton aria-label='likes'>
+					<FavoriteIcon />
+				</IconButton>
+				{likes}
+			</CardContent>
+		</Card>
+	)
+}
